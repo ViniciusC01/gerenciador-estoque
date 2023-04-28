85b6796266ce4427da3af7b2c241d33549f5c90f
ViniciusC01 <viniciuscarvalho134@gmail.com>
2023-04-27 21:42:50 -0300

    d

--- src/estoque/Estoque.java
+++ src/estoque/Estoque.java
@@ -1,6 +1,7 @@
 package estoque;
 
 import entities.Produto;
+import enums.Categoria;
 import item.Item;
 
 import java.util.ArrayList;
@@ -12,12 +13,24 @@ public class Estoque {
 
     ArrayList<Item> listaDeProdutos;
 
-    public void consultarPorCategoriaPreco(){}
-    public void consultarPorCategoria(){}
-    public void adicionarProduto( ){}
-    public boolean removerProduto(Produto produto){
-        for (Item p:listaDeProdutos) {
+    public void consultarPorCategoriaPreco(Categoria ){
 
+    }
+    public Optional<ArrayList<Item>> consultarPorCategoria(Categoria categoria ){
+        ArrayList<Item> itens = new ArrayList<>();
+        for (Item item: listaDeProdutos) {
+            if(item.getCategoria() == categoria){
+                itens.add(item);
+            }
+        }
+        if ()
+        return Optional.of(itens);
+    }
+    public void adicionarProduto( ){}
+    public boolean removerProduto(int id){
+        for (Item p:listaDeProdutos) {
+            if (p.getProduto().getId() == id)
+                listaDeProdutos.remove(p);
         }
 
     }
--- src/item/Item.java
+++ src/item/Item.java
@@ -10,4 +10,36 @@ public class Item {
     BigDecimal quantidade;
     private BigDecimal preco;
     private Categoria categoria;
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public BigDecimal getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 }
